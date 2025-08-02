40b49c3fa9048d43a24a0d5d8723a17e19a38141
pyasmenko88 <pyasmenko@icloud.com>
2025-08-02 21:47:53 +0300

    Добавлен стартовый экран

---
+++ script.js
@@ -0,0 +1,7 @@
+const startScreen = document.getElementById('start-screen');
+const startButton = document.getElementById('start-button');
+
+startButton.addEventListener('click', () => {
+    startScreen.style.display = 'none';
+    // TODO: initialize game
+});
